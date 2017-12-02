cb48ec3f126595016209696b003b82a3aac7db74
pauld <paul.dyatlov@gmail.com>
2017-12-02 17:49:39 +0300

    raft + camera adjustments

--- LD40/Assets/Scripts/CameraController.cs
+++ LD40/Assets/Scripts/CameraController.cs
@@ -1,21 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
-    [SerializeField] private Transform _target;
-    [SerializeField] private float _distance  = 10;
-
-	[SerializeField] private float _smoothSpeed;
+public class CameraController : MonoBehaviour 
+{   
+	[SerializeField] private Transform _target;
 	[SerializeField] private Vector3 _offset;
+	
+	[SerializeField] private float _distance = 10.0f;
+	[SerializeField] private float _height = 5.0f;
+	
+	[SerializeField] private float _heightDamping = 2.0f;
+	[SerializeField] private float _rotationDamping = 3.0f;
 
-	private void Update ()
+	private void LateUpdate () 
 	{
-		var desiredPosition = _target.position + _offset;
-		var smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, _smoothSpeed);
+		var wantedHeight = _target.position.y + _height;
 
-		transform.position = smoothedPosition;
-	    transform.LookAt(_target);
+		var currentRotationAngle = transform.eulerAngles.y;
+		var currentHeight = transform.position.y;
+
+		currentHeight = Mathf.Lerp(currentHeight, wantedHeight, _heightDamping * Time.deltaTime);
+
+		transform.position = _target.position + _offset;
+		transform.position -= Vector3.forward * _distance;
+
+		transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
+		transform.LookAt(_target);
 	}
 }
