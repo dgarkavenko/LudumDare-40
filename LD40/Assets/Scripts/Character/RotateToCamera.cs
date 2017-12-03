11bc2221cf5b49b9abc393ad2dac7c2b695d04d7
pauld <paul.dyatlov@gmail.com>
2017-12-03 11:53:10 +0300

    main scene links corrected

--- LD40/Assets/Scripts/Character/RotateToCamera.cs
+++ LD40/Assets/Scripts/Character/RotateToCamera.cs
@@ -5,6 +5,5 @@ public class RotateToCamera : MonoBehaviour
     private void LateUpdate()
     {
         transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
-        //transform.LookAt(Camera.main.transform);
     }
 }
--- LD40/Assets/Scripts/Debug/CatSpawner.cs
+++ LD40/Assets/Scripts/Debug/CatSpawner.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class CatSpawner : MonoBehaviour
@@ -12,14 +9,14 @@ public class CatSpawner : MonoBehaviour
 
 	private void Start()
 	{
-		/*for (var i = 0; i < 3; i++)
-		{
-			var cat = Instantiate(Links.Instance.Cat, _raft.parent);
-			cat._raft = _raft;
-			var position = Random.insideUnitCircle * 2f;
-			cat.transform.localPosition = new Vector3(position.x, 1.025f, position.y);
-			_main.PickCat(cat);
-		}*/
+//		for (var i = 0; i < 3; i++)
+//		{
+//			var cat = Instantiate(Links.Instance.Cat, _raft.parent);
+//			cat._raft = _raft;
+//			var position = Random.insideUnitCircle * 2f;
+//			cat.transform.localPosition = new Vector3(position.x, 1.025f, position.y);
+//			_main.PickCat(cat);
+//		}
 
 		Raft.OnDrowningCatCollision += OnDrowningCatCollision;
 	}
