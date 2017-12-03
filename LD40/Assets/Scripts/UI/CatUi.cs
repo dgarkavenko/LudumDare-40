588e9219c6b1e5983c536e14a02545fd8b747575
Anton Smetanin <AntonSmetanin@gmail.com>
2017-12-03 23:22:57 +0300

    meow

--- LD40/Assets/Scripts/UI/CatUi.cs
+++ LD40/Assets/Scripts/UI/CatUi.cs
@@ -5,17 +5,20 @@ public class CatUi : MonoBehaviour
 {
     public Cat Cat;
 
-    [SerializeField] private Slider _hangingBar;
+    [SerializeField] private Image _hangingBar;
+    [SerializeField] private Color _startingColor;
+    [SerializeField] private Color _endingColor;
 
     private void Update()
     {
         var hangingCat = Cat.State as Cat.Hanging;
 
-        _hangingBar.gameObject.SetActive(hangingCat != null);
+        _hangingBar.transform.parent.gameObject.SetActive(hangingCat != null);
 
         if (hangingCat != null) {
-            _hangingBar.value = 1f - Mathf.InverseLerp(hangingCat.StartTime, hangingCat.StartTime + Cat.HangingTime, Time.time);
-            _hangingBar.transform.position = Camera.main.WorldToScreenPoint(Cat.transform.position) + new Vector3(20f, 100f);
+            _hangingBar.fillAmount = 1f - Mathf.InverseLerp(hangingCat.StartTime, hangingCat.StartTime + Cat.HangingTime, Time.time);
+            _hangingBar.transform.parent.position = Camera.main.WorldToScreenPoint(Cat.transform.position) + new Vector3(20f, 100f);
+            _hangingBar.color = Color.Lerp(_endingColor, _startingColor, _hangingBar.fillAmount);
         }
     }
 }
