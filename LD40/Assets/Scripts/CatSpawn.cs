e2955084ac5862fe46cd7184d718b97c233b78a1
dgarkavenko <gorrky@yandex.ru>
2017-12-04 23:33:55 +0300

    drowner

--- LD40/Assets/Scripts/CatSpawn.cs
+++ LD40/Assets/Scripts/CatSpawn.cs
@@ -6,6 +6,7 @@ public class CatSpawn : MonoBehaviour
 
     private void Start()
     {
+        transform.position = new Vector3(transform.position.x, 1, transform.position.z);
         Spawner.SpawnCat(transform.position, drowning: true);
     }
 }
