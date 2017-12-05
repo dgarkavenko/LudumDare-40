78d075d103daec2668dd0081faf984e0f1961ab1
dgarkavenko <gorrky@yandex.ru>
2017-12-05 04:06:32 +0300

    raft

--- LD40/Assets/Scripts/Raft/Raft.cs
+++ LD40/Assets/Scripts/Raft/Raft.cs
@@ -124,7 +124,9 @@ public class Raft : FloatingController
 		o.parent = null;
 		o.gameObject.layer = LayerMask.NameToLayer("Хуйня");
 		o.gameObject.AddComponent<BoxCollider>();
-		var r = o.gameObject.AddComponent<Rigidbody>();
+		var r = o.gameObject.GetComponent<Rigidbody>();
+		if(r == null)
+			r = o.gameObject.AddComponent<Rigidbody>();
 		r.AddForce(Vector3.up * 50, ForceMode.Acceleration);
 		r.mass = 2;
 		r.drag = 0.3f;
