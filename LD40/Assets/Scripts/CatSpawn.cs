27dffc9bdc3aa6260e39dcd612a8579e914d6bb7
dgarkavenko <gorrky@yandex.ru>
2017-12-07 00:41:45 +0300

    catspawn instant

--- LD40/Assets/Scripts/CatSpawn.cs
+++ LD40/Assets/Scripts/CatSpawn.cs
@@ -5,14 +5,27 @@ public class CatSpawn : MonoBehaviour
     public CatSpawner Spawner;
     public SphereCollider Collider;
     private bool spawned = false;
+    public bool Instant = false;
     
     private void Start()
     {
+        if (Instant)
+        {
+            enabled = false;
+            Spawner.SpawnCat(transform.position, drowning: true);
+
+        }
+        
         Collider = gameObject.GetComponent<SphereCollider>();
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        
+        if (Instant)
+            return;
+        
         if(spawned)
             return;
         
